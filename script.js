2ade0f82e9768f5a3844641af47fb7a3cdb903bd
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-09-05 13:32:55 +0200

    done

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const ballNode = document.createElement("div"); // se crea la pelotita
 ballNode.id = "ball"; // se asigna un id a la pelotita (para CSS)
 gameBoxNode.append(ballNode); // se añade la pelotita a la caja de juego
 
-// const paddleNode = document.createElement("div"); // se crea la paleta
-// paddleNode.id = "paddle"; // se asigna un id a la paleta (para CSS)
-// gameBoxNode.append(paddleNode); // se añade la pelotita a la caja de juego
+const paddleNode = document.createElement("div"); // se crea la paleta
+paddleNode.id = "paddle"; // se asigna un id a la paleta (para CSS)
+gameBoxNode.append(paddleNode); // se añade la pelotita a la caja de juego
 
 const ball = {
   x: 30,
@@ -17,9 +17,17 @@ const ball = {
   radio: 10,
   speed: 2,
   isMovingRight: true,
-  isMovingDown: true
+  isMovingDown: true,
+  clickandoDerecha: false,
 }
 
+const paddle = {
+  x: 200,
+  y: 550,
+  w: 100,
+  h: 20,
+  speed: 25,
+}
 
 // *** Game Functions ***
 function gameLoop() {
@@ -27,6 +35,7 @@ function gameLoop() {
 
   ballMovement()
   detectColisionBallWall()
+  detectColisionBallPaddle()
 
 }
 
@@ -58,7 +67,8 @@ function detectColisionBallWall() {
   }
 
   if (ball.y >= (gameBoxNode.offsetHeight - ball.h)) {
-    ball.isMovingDown = false
+    // ball.isMovingDown = false
+    gameOver()
   }
 
   if (ball.x <= 0) {
@@ -71,18 +81,61 @@ function detectColisionBallWall() {
 
 }
 
+function gameOver() {
+  ballNode.style.backgroundColor = "red"
+
+  alert("Perdiste el juego :(")
+  clearInterval(gameIntervalId)
+}
+
+function paddleMovement(direction) {
+
+  if (direction === "right") {
+    paddle.x += paddle.speed
+    paddleNode.style.left = `${paddle.x}px`
+  } else if (direction === "left") {
+    paddle.x -= paddle.speed
+    paddleNode.style.left = `${paddle.x}px`
+  }
+
+}
+
+function detectColisionBallPaddle() {
+
+  if ( ((ball.y + ball.h) >= paddle.y) && (ball.x >= paddle.x) && (ball.x <= (paddle.x + paddle.w))  ) {
+    ball.isMovingDown = false
+  }
+
+}
 
 // *** Game Loop Interval ***
 
-setInterval(() => {
+let gameIntervalId = setInterval(() => {
   // console.log("juego andando")
   gameLoop()
 }, 1000/60) // 60fps => 60 veces por segundo
 
 
-
 // *** Event Listeners ***
 
+window.addEventListener("keydown", (event) => {
+  if (event.key === "d") {
+    // console.log("moviendo a la derecha")
+    paddleMovement("right")
+  } else if (event.key === "a") {
+    // console.log("moviendo a la izquierda")
+    paddleMovement("left")
+  }
+})
+
+// para movimiento fluido considera eventos keydown, eventos de keyup, que el movimiento esté en el loop. Pero que el moviento esté condicionado a una variable que cambia con el keydow y keyup.
 
 
+// BONUS
 
+// - la pelota cambia de color al colisionar con la paleta
+// - la pelota cambia de velocidad al colisionar con la paleta o cuando pasado cierto tiempo.
+// - que la pelota se vaya haciendo más pequeña
+// - que aparezca otra pelota
+// - una score que aumenta
+// - limitar el movimiento de la paleta
